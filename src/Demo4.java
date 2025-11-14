001ff5c9929214238202c2a1f4540f35d2b1d73b
skroheedali <skroheedali@gmail.com>
2025-11-14 20:00:35 +0530

    Create Demo4.java

---
+++ src/Demo4.java
@@ -0,0 +1,21 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Done");
+		System.out.println("Roheed Ali");
+		System.out.println("Ali");
+		funA();
+		funB();
+	
+	}
+	public static  void funA()
+	{
+		System.out.println("FunA");
+	}
+	public static  void funB()
+	{
+		System.out.println("FunB");
+	}
+
+}
